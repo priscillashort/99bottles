d50e5f6d75862a38492a5836f9d2ac42dc128bcc
priscillashort <pshort2@cocc.edu>
2020-05-31 18:49:59 -0700

    Final implementation

--- lib/bottles.rb
+++ lib/bottles.rb
@@ -1,29 +1,35 @@
 class Bottles
 
 	def verse(verse_num)
-		return <<-VERSE
-#{verse_num == 0 ? 'No more' : verse_num} bottle#{verse_num == 1 ? '' : 's'} of beer on the wall, #{verse_num == 0 ? 'no more' : verse_num} bottle#{verse_num == 1 ? '' : 's'} of beer.
-#{verse_num == 0 ? 'Go to the store and buy some more, 99' : 'Take ' + (verse_num == 1 ? 'it' : 'one') + ' down and pass it around, ' + (verse_num == 1 ? 'no more' : (verse_num-1).to_s)} bottle#{verse_num-1 == 1 ? '' : 's'} of beer on the wall.
-VERSE
+		verse_output(verse_num)
 	end
 
 	def verses(start_verse, end_verse)
-		return <<-VERSES
-#{multiple_verse_output(start_verse, end_verse)}
-VERSES
+		multiple_verse_output(start_verse, end_verse)
+	end
+
+	def song
+		multiple_verse_output(99, 0)
 	end
 
 	def verse_output(verse_num)
-		"#{verse_num == 0 ? 'No more' : verse_num} bottle#{verse_num == 1 ? '' : 's'} of beer on the wall, #{verse_num == 0 ? 'no more' : verse_num} bottle#{verse_num == 1 ? '' : 's'} of beer.\n
-		#{verse_num == 0 ? 'Go to the store and buy some more, 99' : 'Take ' + (verse_num == 1 ? 'it' : 'one') + ' down and pass it around, ' + (verse_num == 1 ? 'no more' : (verse_num-1).to_s)} bottle#{verse_num-1 == 1 ? '' : 's'} of beer on the wall."
+		"#{verse_num == 0 ? 'No more' : verse_num} bottle#{verse_num == 1 ? '' : 's'} of beer on the wall, #{verse_num == 0 ? 'no more' : verse_num} bottle#{verse_num == 1 ? '' : 's'} of beer.
+#{verse_num == 0 ? 'Go to the store and buy some more, 99' : 'Take ' + (verse_num == 1 ? 'it' : 'one') + ' down and pass it around, ' + (verse_num == 1 ? 'no more' : (verse_num-1).to_s)} bottle#{verse_num-1 == 1 ? '' : 's'} of beer on the wall.
+"
 	end
 
 	def multiple_verse_output(start_verse, end_verse)
 		return_string = '' 
 		current_verse = start_verse
 
-		(start_verse - end_verse).times do
-			return_string = return_string + verse_output(current_verse) + '\n'
+		(start_verse - end_verse + 1).times do
+			if (current_verse == end_verse)
+				return_string = return_string + "#{verse_output(current_verse)}"
+			else
+				return_string = return_string + "#{verse_output(current_verse)}
+"
+			end
+
 			current_verse = current_verse - 1
 		end
 
